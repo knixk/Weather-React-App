38f45a857953d60db2bd1825a9d412ba5eee9370
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2023-09-27 11:32:02 +0530

    changes

--- weather app/src/App.jsx
+++ weather app/src/App.jsx
@@ -1,6 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const url = "https://api.open-meteo.com/v1/forecast?latitude=22.7179&longitude=75.8333&hourly=temperature_2m"
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+
+    const fetchAPI = async () => {
+
+      const req = fetch(url);
+
+      req.then((data) => {
+        console.log(data)
+      })
+
+
+      // fetch(url)
+
+      // const res = req.json()
+
+
+    }
+
+    fetchAPI()
+
+  }, [])
 
   return (
     <>
